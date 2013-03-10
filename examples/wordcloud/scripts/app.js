dadfa5624e909a3cb7a206f5735fc161226c3786
Robin Böhm <robinboehm@googlemail.com>
2013-03-10 21:09:56 +0100

    Added example template

--- examples/wordcloud/scripts/app.js
+++ examples/wordcloud/scripts/app.js
@@ -4,7 +4,19 @@ angular.module('examplesApp', ['d3'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        template: '<wordcloud words="words"></wordcloud>',
+            // Only for example in the route provider
+        template: '' +
+            '' +
+            '<h1>WordCloud D3 Directive</h1>' +
+            '<h2>By Robin Böhm</h2>' +
+            '' +
+            '<h3>Simple Words</h3>' +
+            '<wordcloud words="words"></wordcloud>' +
+            '<h3>Adjustable size</h3>' +
+            '<wordcloud words="words" width="300" height="300"></wordcloud>' +
+            '<h3>Font-Family and -Size attributes</h3>' +
+            '<wordcloud words="words" font-size="10" font-family="Arial"></wordcloud>' +
+            '',
         controller: 'MainCtrl'
       })
       .otherwise({
